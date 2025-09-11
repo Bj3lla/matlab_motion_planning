73f3b1168df4fe12c6c96bef022c3ece84307b47
Jonas Berger Nyvold <jonas.nyvold02@gmail.com>
2025-09-11 16:45:55 +0200

    3d model

--- utils/env/generate_grid.m
+++ utils/env/generate_grid.m
@@ -1,15 +1,34 @@
-function grid_map = generate_grid(size, obstacle)
-%%
-% @file: generate_grid.m
-% @breif: generate grid map
-% @author: Winter
-% @update: 2023.1.29
+function grid_map = generate_grid(map_size, obstacle)
+%% generate_grid
+% map_size: [rows cols heights]
+% obstacle:
+%   - [] or omitted -> no obstacles
+%   - vector of linear indices into grid_map
+%   - OR an N-by-3 list of [row col height] (1-based subscripts)
+%
+% Encoding:
+%   1 = empty
+%   2 = obstacle
 
-%%
-% 1 ------ empty
-% 2 ------ obstacle
+    arguments
+        map_size (1,3) {mustBeInteger, mustBePositive}
+        obstacle = []
+    end
 
-%%
-    grid_map = ones(size(1), size(2));
-    grid_map(obstacle) = 2;
+    % Initialize all empty
+    grid_map = ones(map_size);  % 1 = empty
+
+    if isempty(obstacle)
+        return;
+    end
+
+    % Accept either linear indices or [row col height] subscripts
+    if size(obstacle,2) == 3
+        linIdx = sub2ind(map_size, obstacle(:,1), obstacle(:,2), obstacle(:,3));
+    else
+        linIdx = obstacle(:);
+    end
+
+    % Place obstacles
+    grid_map(linIdx) = 2;  % 2 = obstacle
 end
